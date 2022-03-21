1a5a146b61ad446da60aec7561efa8b54eaa8533
sapirowsky <sapirowsky@gmail.com>
2022-03-21 21:50:49 +0100

    no zaczyna to dzialac

--- assets/js/content/main.js
+++ assets/js/content/main.js
@@ -1,61 +1,67 @@
 import { renderElements, renderList } from "./render.js";
-import imgList from './img-list.json' assert {type:"json"} 
+import imgList from './data/img-list.json' assert {type:"json"} 
+import arrayOfTechnologyContent from './data/technology-content.json' assert {type:"json"} 
+import arrayOfRealizationContent from './data/realization-content.json' assert {type:"json"} 
+import arrayOfOfferContent from './data/offer-content.json' assert {type:"json"} 
+import arrayOfContactContent from './data/contact-content.json' assert {type:"json"} 
 
-
-
-// Dwuwymiarowe tablice z zawartością podstron:
-// tablica = [[typ, content], [typ, content]...]
-const arrayOfTechnologyContent = [
-    ["h1", "Technologia"],
-    ["h3", "Panoramy sferyczne 360x180"],
-    ["p", 'Wirtualne wycieczki oferowane przez nas są połączeniem panoram sferycznych o kącie widzenia 360 stopni horyzontalnie oraz 180 stopni w pionie. Oznacza to, iż możliwe jest rozglądanie się po panoramie w każdym kierunku. Uwiecznione są również "sufit" i "podłoga", dzięki czemu można przedstawić wszystkie walory architektoniczne otoczenia. Kładziemy najsilniejszy nacisk na poprawne sklejanie fotografii w gotową panoramę z uwzględnieniem kolorystyki i jasności poszczególnych fotografii, dzięki czemu scena wygląda jak jedno spójne zdjęcie.'],
-    ["h3", "Flash i HTML5"],
-    ["p", 'Do tej pory najbardziej uniwersalną i najbardziej rozpowszechnioną technologią osadzania grafiki interaktywnej była technologia Flash. Jednakże nie była ona poprawnie wyświetlana na wszystkich urządzeniach lub nie była w ogóle wspierana (np. wszystkie systemy iOS ) oraz zawierała wiele błędów i luk bezpieczeństwa, przez co ogólnoświatowe trendy nie planują dalszego rozwoju tej technologii. Za obecny standard uważa się technologię HTML5, która jest bardzo prężnie wdrażana w największych serwisach (np. Youtube, Facebook, Twitter). Dzięki zastosowaniu przez nas playera KRPano 1.18.2, który wykorzystuje obydwie wspomniane technologie, oferowane usługi będą bezproblemowo wyświetlane na wszystkich urządzeniach i przeglądarkach.'],
-    ["h3", "Wysoka szczegółowość obrazu i HDR"],
-    ["p", 'Wiele firm oferujących wykonanie wirtualnej wycieczki tworzy je za pomocą obiektywu ultraszerokokątnego (tzw. Fisheye), dzięki czemu wykonanie panoramy jest bardzo proste i szybkie, gdyż na pełną panoramę 360x180 (pełna sfera) wystarcza 6 zdjęć. Takie podejście jednak skutkuje niską jakością panoramy. Bardzo mała rozdzielczość nie umożliwia wystarczającego powiększana wybranego fragmentu panoramy, a wady optyczne obiektywu fisheye dodatkowo pogarszają jakość obrazu. Nasze panoramy wykonujemy z co najmniej 26 zdjęć, co po połączeniu daje obraz o rozdzielczości około 22 000 x 11 000 pixeli, co zapewnia bardzo wysoką jakość obrazu i wysoką swobodę przybliżania panoramy. Na specjalne życzenie możliwe jest wykonanie panoram o dużo większej rozdzielczości.'],
-    ["p", 'W szczególnie trudnych przypadkach możliwe jest zastosowanie technologii HDR. Umożliwi to poprawne oddanie charakteru oświetlenia danej panoramy.']
-]
-const arrayOfOfferContent = [
-    ["h1", "Oferta"],
-    ["p", 'W odróżnieniu od większości firm oferujących wykonanie wirtualnej wycieczki, nasze usługi nie są ukierunkowane na jednorazowy, spory zysk lecz na stałą współpracę. Podstawową usługą oferowaną przez nas jest stała opieka nad Państwa wirtualną wycieczką i obejmuje jej wykonanie oraz coroczną aktualizację oraz osadzenie na naszym serwerze. Wycieczka składająca się z ok. 15 panoram, może zajmować nawet 1,5GB, a wykupienie hostingu tak dużej przestrzeni serwerowej może sporo kosztować. Dzięki osadzeniu wirtualnej wycieczki na naszym serwerze, nie muszą się Państwo o to martwić. Dostęp do niej odbywa się przez Państwa stronę (za pomocą przekierowania) oraz przez naszą stronę, co stanowi dla Państwa dodatkową reklamę. Kolejnym udogodnieniem jest abonamentowa forma opłaty za stałą opiekę i aktualizację wirtualnej wycieczki oraz przestrzeń serwerową. Gdyby taka forma opłaty Państwa nie satysfakcjonowała, jest możliwość tradycyjnej jednorazowej opłaty za wykonanie wirtualnej wycieczki. Dodatkowo oferujemy wykupienie samej przestrzeni serwerowej. Dobrą formą promocji firmy mogą być gadżety upominkowe w formie pendriva lub płyty DVD z wirtualną wycieczką, które mogli by Państwo ofiarować swoim klientom.'],
-    ["p", 'Aby zaspokoić potrzeby wszystkich klientów, ceny są ustalane indywidualnie po wcześniejszym kontakcie drogą mailową.']
-]
-const arrayOfContactContent = [
-    ["h1", 'Kontakt:'],
-    ["p", 'Krystian Firla'],
-    ["p", 'Email: kfirla@gmail.com'],
-    ["p", 'nr kom: 698 490 438']
-]
+if(localStorage.getItem('site') == null) localStorage.setItem('site', 'technology')
 
 // Nasłuchiwanie kliknięcia i zmienianie zawartości strony 
 const technology = document.querySelector("#technology")
 technology.addEventListener('click', () => {
     renderElements(".main-content", arrayOfTechnologyContent)
+
+    localStorage.site = "technology"
+})
+
+const realization = document.querySelector("#realization")
+realization.addEventListener('click', () => {
+    renderElements(".main-content", arrayOfRealizationContent)
+
+    const imgListLocationLibrary = document.querySelector("#library")
+    renderList(imgList, "p", imgListLocationLibrary)
+
+    localStorage.site = "realization"
 })
 
 const offer = document.querySelector("#offer")
 offer.addEventListener('click', () => {
     renderElements(".main-content", arrayOfOfferContent)
+
+    localStorage.site = "offer"
 })
 
 const contact = document.querySelector("#contact")
 contact.addEventListener('click', () => {
     renderElements(".main-content", arrayOfContactContent)
+
+    localStorage.site = "contact"
+})
+
+const imgListLocation = document.querySelector("#left-img-list")
+renderList(imgList, "h1", imgListLocation)
+
+
+document.querySelector("#logo-link").addEventListener("click", () => {
+    renderElements(".main-content", arrayOfTechnologyContent)
 })
 
 
-// Trzywymiarowa tablica z wszystkimi zdjeciami i opisami
-// tablica = [ [ [img, src, alt], [typ, alternatywnyTyp, zawartośc] ], [ [img, src, alt], [typ, alternatywnyTyp, zawartośc] ]... ]
-// const arrayWithImg = [
-//     [
-//         ["img", "assets/img/screenshot.png", "jakies zdjecie"],
-//         ["h1", "p", "Przykładowe miejsce #1"]
-//     ],
-//     [
-//         ["img", "assets/img/Beautiful-Full-HD-Images.jpg", "jakies zdjecie"],
-//         ["h1", "p", "Przykładowe miejsce #2"]
-//     ]
-// ]
+let latestPage = localStorage.getItem("site")
+
+if(latestPage === "technology") renderElements(".main-content", arrayOfTechnologyContent)
+if(latestPage === "realization"){
+    renderElements(".main-content", arrayOfRealizationContent)
+
+    const imgListLocationLibrary = document.querySelector("#library")
+    renderList(imgList, "p", imgListLocationLibrary)
+}
+if(latestPage === "offer") renderElements(".main-content", arrayOfOfferContent)
+if(latestPage === "contact") renderElements(".main-content", arrayOfContactContent)
 
 
-console.log(imgList)
+
+
+
+
--- assets/js/content/render.js
+++ assets/js/content/render.js
@@ -7,11 +7,40 @@ function renderElements(where, arrayOfContent) {
 
     arrayOfContent.forEach(myArray => {
         const element = document.createElement(myArray[0])
+        if(myArray[0] === "ul") element.setAttribute("id", "library")
         element.textContent = myArray[1]
         content.appendChild(element)
     })
 }
 
-function renderList(){
 
+// Funkcja służy do renderewania elementów listy 
+function renderList(items, marker, location){
+    if(items){
+        location.innerHTML = ""
+
+        items.forEach((item, i) => {
+            const liElement = document.createElement("li")
+            if(i === 0) liElement.dataset.active = true
+            if(marker === "h1") liElement.classList.add("slide")
+
+            const aElement = document.createElement("a")
+            aElement.setAttribute("href", item[0][0])
+            
+            const imgElement = document.createElement("img")
+            imgElement.setAttribute("src", item[1][0])
+            imgElement.setAttribute("alt", item[1][1])
+
+
+            const textElement = document.createElement(marker)
+            textElement.textContent = item[2][0]
+
+
+            aElement.appendChild(imgElement)
+            aElement.appendChild(textElement)
+
+            liElement.appendChild(aElement)
+            location.appendChild(liElement)
+        })
+    }
 }
